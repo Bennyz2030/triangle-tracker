6529922f698c95a40ae20b2c1a6bd8ce367a7574
NIck <nicholasbenzinger@gmail.com>
2021-08-12 16:32:51 -0700

    add required to form field
    Co-authored by Scott Hutley <scotthutley1@comcast.net>

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,15 @@ $(document).ready(function() {
     const side1 = parseInt($("input#side1").val());
     const side2 = parseInt($("input#side2").val());
     const side3 = parseInt($("input#side3").val());
-
+    let result;
     if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
-      $("span#result").text("Not a Triangle")
+      $("#result").text("Not a Triangle");
     } else if (side1 === side2 && side2 === side3) {
-      $("span#result").text("Equilateral")
+      $("#result").text("Equilateral");
     } else if (side1 === side2 || side2 === side3 || side3 === side1) {
-      $("span#result").text("Isosceles")
+      $("#result").text("Isosceles");
     } else if (side1 !== side2 || side2!== side3 || side3 !== side1) {
-      $("span#result").text("Scalene")      
-    }
+      $("#result").text("Scalene");    
+    } 
   });
 });
